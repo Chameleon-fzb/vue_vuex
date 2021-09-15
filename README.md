da23b6794bb5d76e4c7286bdc269d072eb2014de
chameleon-fzb <1466518303@qq.com>
2021-09-15 13:59:31 +0800

    第四次提交

--- README.md
+++ README.md
@@ -1,24 +1,8 @@
-# vue_vuex
+# vue_Vuex
 
-## Project setup
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## 使用场景
+1. 多个组件依赖同一个状态
+2. 来自不同组件的行为需要变更同一状态
+3. 多个组件要共享状态
+  Vuex store.js
+  store index.js
---
+++ src/vuex/store.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+//? 1.初始化状态, 要写成一个对象, 因为该state要管理n 多个组件的状态
+
+const state = {
+	sum: 0
+}
+//? 2.创建一个actions , 值为一个对象 包含:n个响应组件"动作"的函数
+const actions = {
+	increase(context, value) {
+		console.log(value)
+		context.commit('INCREASE', value)
+	},
+	decrease(context, value) {
+		context.commit('DECREASE', value)
+	},
+	increaseAsync(context, value) {
+		setTimeout(() => {
+			context.commit('INCREASE_ASYNC', value)
+		}, 500)
+	}
+}
+const mutations = {
+	INCREASE(state, value) {
+		state.sum += value
+	},
+	DECREASE(state, value) {
+		state.sum -= value
+	},
+	INCREASE_ASYNC(state, value) {
+		state.sum += value
+	}
+}
+// 创建store 用于管理: state 对象 actions 对象 mutations 对象
+const store = new Vuex.Store({ state, actions, mutations })
+export default store
