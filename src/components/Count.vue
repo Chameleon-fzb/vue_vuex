248b0a1b7e66da830c424cf23aaebbeb39d9a150
chameleon-fzb <1466518303@qq.com>
2021-09-15 20:40:31 +0800

    getters

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h2>当前求和为: {{$store.state.sum}}</h2>
+    <h2>当前求和处理后结果为: {{$store.getters.bigSum}}</h2>
     <select v-model.number="n">
       <option value="1">1</option>
       <option value="2">2</option>
@@ -8,7 +9,7 @@
     </select><br/>
     <button @click="increase">+</button>&nbsp;
     <button @click="decrease">-</button>&nbsp;
-    <button @click="increaseAsync">异步加</button>&nbsp;
+<button @click="increaseAsync">异步加</button>&nbsp; 
     <button @click="increaseOdd">奇数加</button>
   </div>
 </template>
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
 
-//? 1.初始化状态, 要写成一个对象, 因为该state要管理n 多个组件的状态
+//! 1.初始化状态, 要写成一个对象, 因为该state要管理n 多个组件的状态
 
 const state = {
 	sum: 0
 }
-//? 2.创建一个actions , 值为一个对象 包含:n个响应组件"动作"的函数
+//! 2.创建一个actions , 值为一个对象 包含:n个响应组件"动作"的函数
 const actions = {
 	increase(context, value) {
 		// ?context 是一个mini版的$store 要使用context.commit()去通知mutations加工状态
@@ -29,6 +29,7 @@ const actions = {
 		}
 	}
 }
+//! 3创建一个mutations 值为一个对象,包含 : n个真正用于加工状态的函数
 const mutations = {
 	INCREASE(state, value) {
 		state.sum += value
@@ -43,6 +44,13 @@ const mutations = {
 		state.sum += value
 	}
 }
-// 创建store 用于管理: state 对象 actions 对象 mutations 对象
-const store = new Vuex.Store({ state, actions, mutations })
+// ?getters中配置的是state中的数据经过加工后的属性 -- 类似于vue 中的计算属性
+const getters = {
+	bigSum(state) {
+		return state.sum * 100
+	}
+}
+
+//!4 创建store 用于管理: state 对象 actions 对象 mutations 对象
+const store = new Vuex.Store({ state, actions, mutations, getters })
 export default store
